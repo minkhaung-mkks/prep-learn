15843c6bedbe9e9721d6c7a4a2c6f5313923b640
Min Khaung <minkhaung.mkks@gmail.com>
2023-05-18 22:51:17 +0630

    Made the data persists through navigation and reloads

--- app/api/questions/route.js
+++ app/api/questions/route.js
@@ -1,6 +1,5 @@
 import questions from '@dummydata/dummyData.json';
 
 export const GET = async (req, res) => {
-    console.log(questions)
     return new Response(JSON.stringify(questions), { status: 200 })
 };
--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,8 +1,9 @@
 "use client"
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 export default function QuestionsPage() {
     const [sections, setSections] = useState([]);
+    const firstUpdate = useRef(true);
     const [answers, setAnswers] = useState({});
     const [showScore, setShowScore] = useState(false);
     const [score, setScore] = useState(null);
@@ -14,48 +15,41 @@ export default function QuestionsPage() {
             const data = await res.json();
             setSections(data.Sections);
 
-            /**
-             * This code initializes the `initialAnswers` object which will hold the user's answers to the questions.
-             * 
-             * 1. The `reduce` method is being used on `data.Sections` which is an array of all sections in the data. `reduce` produces a single output value from the array's elements. This output value could be any data type.
-             * 
-             * 2. The `reduce` method takes two arguments: a callback function and an initial value. The callback function also takes two arguments: an accumulator (`acc`) and the current value being processed (`section` and `i` which is its index in the array).
-             * 
-             * 3. In each iteration, the callback function returns a new object that contains all properties of the accumulator object (i.e., `...acc`) and a new property. This new property's key is the index of the current section (`[i]`), and its value is another object.
-             * 
-             * 4. This new object is created by reducing over the `questions` array of the current section. Similar to the outer `reduce`, 
-             * it returns an object that contains an empty string for each question (indicating that the user hasn't answered the question yet). 
-             * 
-             * 5. The `acc, _, j` parameters in the inner `reduce` function represents the accumulator (the object being built),
-             *  the current question (which is ignored, hence the `_`), and the index of the current question (`j`), respectively.
-             * 
-             * 6. The resulting `initialAnswers` object will be something like this (assuming there are two sections and each section has two questions):
-             * 
-             * {
-             *   0: { 0: '', 1: '' },
-             *   1: { 0: '', 1: '' }
-             * }
-             * 
-             * Each key in the outer object represents a section, 
-             * and its value is another object where each key represents a question in that section and 
-             * its value is the user's answer to that question ('' indicating no answer).
-            */
             let totalQuestions = 0;
             data.Sections.forEach((section) => {
                 totalQuestions += section.questions.length;
             });
-            const initialAnswers = data.Sections.reduce(
-                (acc, section, i) => ({
-                    ...acc,
-                    [i]: section.questions.reduce(
-                        (acc, _, j) => ({ ...acc, [j]: '' }),
-                        {}
-                    ),
-                }),
-                {}
-            );
+
+            let initialAnswers;
+            const savedAnswers = localStorage.getItem('answers');
+            if (savedAnswers) {
+                initialAnswers = JSON.parse(savedAnswers);
+            }
+            else {
+                /**
+                    * Initializes `initialAnswers` to store user's answers.
+                    * 
+                    * 1. `reduce` is used on `data.Sections` to create an object from the array.
+                    * 2. In each iteration, the callback returns a new object that merges the accumulator object (`acc`) and a new property, where key is the section index (`[i]`), and value is another object.
+                    * 3. The value object is created by reducing the `questions` array of the current section. It returns an object with an empty string for each unanswered question. 
+                    * 4. `acc, _, j` in the inner `reduce` represents the accumulator (object being built), the current question (ignored - `_`), and the question index (`j`).
+                    * 5. `initialAnswers` structure is { sectionIndex: { questionIndex: answer }, ...}, where answer is an empty string for an unanswered question.
+                */
+                initialAnswers = data.Sections.reduce(
+                    (acc, section, i) => ({
+                        ...acc,
+                        [i]: section.questions.reduce(
+                            (acc, _, j) => ({ ...acc, [j]: '' }),
+                            {}
+                        ),
+                    }),
+                    {}
+                );
+            }
+
+
+            console.log(initialAnswers)
             setTotalScore(totalQuestions)
-            console.log(totalQuestions)
             setAnswers(initialAnswers);
         };
 
@@ -88,6 +82,16 @@ export default function QuestionsPage() {
         setShowScore(true);
     };
 
+    useEffect(() => {
+        // Skip on first render
+        if (firstUpdate.current) {
+            firstUpdate.current = false;
+            return;
+        }
+
+        // Save to local storage whenever answers change
+        localStorage.setItem('answers', JSON.stringify(answers));
+    }, [answers]);
 
     return (
         <div>
