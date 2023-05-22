09602c7f3a11af698485f57f7269632009baf116
Min Khaung <minkhaung.mkks@gmail.com>
2023-05-22 15:27:56 +0630

    Dummy pages because next runs too slow on my pc

--- app/page.jsx
+++ app/page.jsx
@@ -4,8 +4,8 @@ const page = () => {
     // Home page should have the Name of the App and the tagline
     // It should also include the intention behind the app.
     return (
-        <section className="home_page">
-
+        <section className="hero_box">
+            
         </section>
     )
 }
