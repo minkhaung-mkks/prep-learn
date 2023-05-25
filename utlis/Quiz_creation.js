884299cc0180364617f7b70832a0638edf8509d6
Min Khaung <minkhaung.mkks@gmail.com>
2023-05-25 12:04:53 +0630

    Fixed Answers not being parsed correctly

--- utlis/Quiz_creation.js
+++ utlis/Quiz_creation.js
@@ -36,17 +36,21 @@ const parseAnswers = (answersText) => {
     // Initialize variables to hold the correct answer and the reason
     let correctAnswer = '';
     let reason = '';
+    let correctAnswers =[]
+    let reasons = []
 
     // Loop over each line
     for (let line of answerLines) {
         reason = ''
         // Try to match the line to the pattern for a correct answer (e.g., "1. a. Some text")
-        let match = line.match(/^\s*\d\.\s*(\w)\.\s*(.*)\s*$/);
+        let match = line.match(/^\s*\d+\.\s*(\w)\.\s*(.*)\s*$/);
+        console.log(match)
         if (match) {
             // If the line matches, extract the option letter and the answer text
             const [_, option, text] = match;
             // Set `correctAnswer` to the option letter and answer text combined (e.g., "a. Some text")
             correctAnswer = `${option}. ${text}`;
+            correctAnswers.push(correctAnswer)
         }
 
         // Try to match the line to the pattern for a reason (e.g., "- Some text")
@@ -56,13 +60,14 @@ const parseAnswers = (answersText) => {
             const [_, reasonText] = match;
             // Set `reason` to the reason text
             reason = reasonText;
+            reasons.push(reason)
         }
     }
 
     // Return the correct answer and the reason
     return {
-        correctAnswer,
-        reason,
+        correct: correctAnswers,
+        reasons,
     };
 }
 
@@ -78,10 +83,109 @@ const txtToData = (context, question, answer = null) => {
     // Update answers for questions
     let answers = parseAnswers(answer);
     for (let i = 0; i < section.questions.length; i++) {
-        if (answers[i + 1]) {
-            section.questions[i].correct = answers[i + 1];
+        if (answers.correct[i]) {
+            section.questions[i].correct = answers.correct[i];
         }
     }
-    console.log(section);
+    console.log(section)
     return section;
 }
+
+let c = ""
+let q = `
+1. A system where a single authority, like a monarch, exercises absolute power is known as:
+   a. Democracy
+   b. Oligarchy
+   c. Autocracy
+   d. Republic
+
+2. The United States government is best characterized as a:
+   a. Direct democracy
+   b. Theocracy
+   c. Federal republic
+   d. Monarchy
+
+3. In which system of government is power divided between a central government and individual states or provinces?
+   a. Unitary system
+   b. Federal system
+   c. Confederacy
+   d. Oligarchy
+
+4. What type of government does the United Kingdom have?
+   a. Absolute monarchy
+   b. Constitutional monarchy
+   c. Direct democracy
+   d. Presidential system
+
+5. In a parliamentary system of government, who typically holds the most power?
+   a. The Queen/King
+   b. The President
+   c. The Prime Minister
+   d. The Judiciary
+
+6. Which system of government is characterized by rule by a few, often the wealthy or well-educated?
+   a. Democracy
+   b. Monarchy
+   c. Oligarchy
+   d. Autocracy
+
+7. In which type of government do religious laws form the legal system?
+   a. Theocracy
+   b. Federal system
+   c. Republic
+   d. Democracy
+
+8. Which country is an example of a constitutional monarchy?
+   a. Canada
+   b. United States
+   c. France
+   d. China
+
+9. In a direct democracy, decisions are made by:
+   a. The President
+   b. The citizens directly
+   c. Elected representatives
+   d. A Monarch
+
+10. A government where power is held by one central authority is known as:
+   a. Federal
+   b. Unitary
+   c. Confederation
+   d. Anarchy
+
+11. The head of state in a parliamentary system is often:
+   a. The Prime Minister
+   b. The President
+   c. The Queen/King
+   d. The Chief Justice
+
+12. A type of government where the people govern themselves without any intermediary is a:
+   a. Direct democracy
+   b. Indirect democracy
+   c. Monarchy
+   d. Republic
+
+13. Who has the power in a representative democracy? *
+   a. Elected officials
+   b. All citizens
+   c. The monarch
+   d. The military   
+`
+
+let a = `
+1. c. Autocracy
+2. c. Federal republic
+3. b. Federal system
+4. b. Constitutional monarchy
+5. c. The Prime Minister
+6. c. Oligarchy
+7. a. Theocracy
+8. a. Canada
+9. b. The citizens directly
+10. b. Unitary
+11. c. The Queen/King
+12. a. Direct democracy
+13. a. Elected officials
+`
+
+txtToData(c,q,a)
