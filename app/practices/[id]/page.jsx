19eff0a8333597e53ce97fe14907afdbf8e01f5c
Min Khaung <minkhaung.mkks@gmail.com>
2023-06-11 22:07:31 +0630

    Fetch Quiz done

--- app/practices/[id]/page.jsx
+++ app/practices/[id]/page.jsx
@@ -4,41 +4,57 @@ import '@styles/global.css'
 import '@styles/quiz.css'
 
 const PraticePage = () => {
-    const [questions, setQuestions] = useState([])
+    const [quiz, setQuiz] = useState([])
+    const quizId = parseInt(params.id);
     const submitTest=()=>{
 
     }
+    useEffect(()=>{
+        const retriveData = async ()=>{
+            try {
+                const response = await fetch('../../dummydata/dummyData.json');
+                const data = await response.json();
+
+                data = data.filter(quiz=> quiz.id === quizId)
+                setQuiz(data);
+            } catch (error) {
+                console.error('Error fetching quiz data: ', error);
+            }
+        }
+        retriveData();
+    
+    },[])
     return (
         <main id="web_page">
         <div class="quiz_info_box">
 
         </div>
-        <form onsubmit={submitTest} class="questions_box">
-            ${questions.sections.map((section) => {
+        <form onsubmit={submitTest} class="quiz_box">
+            {quiz.sections.map((section, index) => {
                 <>
                 <h2 class="section_title">
-                    Section 
+                    Section {index+1}
                 </h2>
                 <div class="context_box">
                     <p class="contenxt">
-                        ${section.context}
+                        {section.context}
                     </p>
                 </div>
-                ${
-                    section.questions.map((question)=>{
+                {
+                    section.quiz.map((question)=>{
                         <div class="question_card">
                             <h3 class="question">
-                                ${question.text}
+                                {question.text}
                             </h3>
                             <div class="answers_box">
                                 <input class="answers" type="radio" name={question.text} id="A"/>
-                                <label for="A">${questions.answers.a}</label>
+                                <label for="A">{quiz.answers.a}</label>
                                 <input class="answers" type="radio" name={question.text} id="B"/>
-                                <label for="B">${questions.answers.b}</label>
+                                <label for="B">{quiz.answers.b}</label>
                                 <input class="answers" type="radio" name={question.text} id="C"/>
-                                <label for="C">${questions.answers.c}</label>
+                                <label for="C">{quiz.answers.c}</label>
                                 <input class="answers" type="radio" name={question.text} id="D"/>
-                                <label for="D">${questions.answers.d}</label>
+                                <label for="D">{quiz.answers.d}</label>
                             </div>
                         </div>
                     })
