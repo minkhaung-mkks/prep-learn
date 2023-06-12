cc3f0a699a99a5264f04d340c4f9b951faca6413
Min Khaung <minkhaung.mkks@gmail.com>
2023-06-12 22:51:31 +0630

    Changed QuizInfo into its own component card

--- app/practices/page.jsx
+++ app/practices/page.jsx
@@ -3,6 +3,7 @@ import React,{useEffect,useState} from 'react'
 import Image from 'next/image'
 import Link from 'next/link';
 import { useRouter } from "next/navigation";
+import InfoCard from '@components/InfoCard';
 
 
 const PracticeListPage = () => {
@@ -219,49 +220,7 @@ const PracticeListPage = () => {
        
         <div class="quiz_select_box">
             {quizzes.map((quiz, index)=>(
-                <div key={index} class="quiz_card">
-                
-                <div class="creator_info_box">
-                    <Image src="../public/assets/preplearn-website-favicon-color.png" alt="" class="creator_img"/>
-                    
-                    <a href={`../users/{quiz.creatorID}`} class="creator_name">{quiz.creator}</a>
-                    <h5 class="creator_type"></h5>
-                    <h4 class="quiz_type"> {quiz.source}</h4>
-                </div>
-                
-                <div class="quiz_info_box">
-                    
-                    <div class="quiz_title_box">
-                       
-                        <h2 class="quiz_title">{quiz.name}</h2>
-                       
-                        <h3 class="quiz_subject">Subject : {quiz.subject}</h3>
-                        
-                        <h3 class="quiz_topic">Topic : {quiz.topic}</h3>
-                        {quiz.sub_topic !== "" && 
-                            <h3 class="quiz_topic">Sub Topic {quiz.sub_topic}</h3>
-                        }
-                    </div>
-                    
-                </div>
-                <div class="quiz_info_box">
-                    
-                    <div class="quiz_title_box">
-                        
-                        <h3 class="quiz_title">Difficulty : {quiz.difficulty}</h3>
-                       
-                        <h3 class="quiz_subject">Questions : {quiz.total_questions} questions</h3>
-                        
-                        <h3 class="quiz_topic"> Ideal Time : {quiz.ideal_time} Mins</h3>
-                    </div>
-                    
-                </div>
-                <div class="take_quiz_btn_box">
-                    <button onClick={takeQuiz(quiz.id)} class="take_quiz_btn">
-                        Start
-                    </button>
-                </div>
-            </div>
+                <InfoCard key={index} quiz={quiz}/>
             ))}
             
         </div>
--- components/InfoCard.jsx
+++ components/InfoCard.jsx
@@ -1,8 +1,51 @@
 import React from 'react'
+import Image from 'next/image'
 
-const InfoCard = () => {
+const InfoCard = ({index,quiz}) => {
     return (
-        <div>InfoCard</div>
+        <div class="quiz_card">
+                
+                <div class="creator_info_box">
+                    <Image src="../public/assets/preplearn-website-favicon-color.png" alt="" class="creator_img"/>
+                    
+                    <a href={`../users/{quiz.creatorID}`} class="creator_name">{quiz.creator}</a>
+                    <h5 class="creator_type"></h5>
+                    <h4 class="quiz_type"> {quiz.source}</h4>
+                </div>
+                
+                <div class="quiz_info_box">
+                    
+                    <div class="quiz_title_box">
+                       
+                        <h2 class="quiz_title">{quiz.name}</h2>
+                       
+                        <h3 class="quiz_subject">Subject : {quiz.subject}</h3>
+                        
+                        <h3 class="quiz_topic">Topic : {quiz.topic}</h3>
+                        {quiz.sub_topic !== "" && 
+                            <h3 class="quiz_topic">Sub Topic {quiz.sub_topic}</h3>
+                        }
+                    </div>
+                    
+                </div>
+                <div class="quiz_info_box">
+                    
+                    <div class="quiz_title_box">
+                        
+                        <h3 class="quiz_title">Difficulty : {quiz.difficulty}</h3>
+                       
+                        <h3 class="quiz_subject">Questions : {quiz.total_questions} questions</h3>
+                        
+                        <h3 class="quiz_topic"> Ideal Time : {quiz.ideal_time} Mins</h3>
+                    </div>
+                    
+                </div>
+                <div class="take_quiz_btn_box">
+                    <button onClick={takeQuiz(quiz.id)} class="take_quiz_btn">
+                        Start
+                    </button>
+                </div>
+            </div>
     )
 }
 
