3bcb2b83617850bc626e3e9609edd0bbacdfa2c3
Min Khaung <minkhaung.mkks@gmail.com>
2023-06-14 21:21:39 +0630

    Changed Class to className

--- app/practices/page.jsx
+++ app/practices/page.jsx
@@ -4,6 +4,8 @@ import Image from 'next/image'
 import Link from 'next/link';
 import { useRouter } from "next/navigation";
 import InfoCard from '@components/InfoCard';
+import '@styles/global.css'
+import '@styles/quiz_chooser.css'
 
 
 const PracticeListPage = () => {
@@ -13,9 +15,7 @@ const PracticeListPage = () => {
     const [selectedTopics, setSelectedTopics] = useState([]); 
     const [selectedSubTopics, setSelectedSubTopics] = useState([]); 
 
-  const router = useRouter();
-
-
+  
     
     const selectSubTopics = (sub_topic) =>{
         // When provided a function on setState, react calls the function with the current State
@@ -40,14 +40,13 @@ const PracticeListPage = () => {
         }
     }
 
-    const takeQuiz = (id)=>{
-        router.push(`/practices/${id}`)
-    }
+    
     useEffect(()=>{
         const retriveData = async ()=>{
             try {
-                const response = await fetch('../../dummydata/dummyData.json');
-                const data = await response.json();
+                const response = await fetch('dummydata/dummyData.json');
+                let data = await response.json();
+                console.log(data)
     
                 // Filter by selected subject if it's not 'All'
                 if (selectedSubject !== 'All') {
@@ -75,103 +74,103 @@ const PracticeListPage = () => {
     },[selectedSubject,selectedTopics,selectedSubTopics])
     return (
         <main id="web_page">
-        <h2 class="headings">Exam</h2>
-        <div class="heading_line"></div>
-        <div class="selector_btns_box">
-            <button data-exam="GED" class="selected_selector_btn selector_btn">
+        <h2 className="headings">Exam</h2>
+        <div className="heading_line"></div>
+        <div className="selector_btns_box">
+            <button data-exam="GED" className="selected_selector_btn selector_btn">
                 GED
             </button>
         </div>
-        <h2 class="headings">Subject</h2>
-        <div class="heading_line"></div>
-        <div id="GED" class="selector_btns_box">
-            <button onClick={setSelectedSubject('All')} class="selected_selector_btn selector_btn">
+        <h2 className="headings">Subject</h2>
+        <div className="heading_line"></div>
+        <div id="GED" className="selector_btns_box">
+            <button onClick={()=>setSelectedSubject('All')} className="selected_selector_btn selector_btn">
                 All
             </button>
-            <button  onClick={setSelectedSubject('Social Studies')} class="selector_btn">
+            <button  onClick={()=>setSelectedSubject('Social Studies')} className="selector_btn">
                 Social Studies
             </button>
-            <button  onClick={setSelectedSubject('RLA')} class="selector_btn">
+            <button  onClick={()=>setSelectedSubject('RLA')} className="selector_btn">
                 Reasoning through Art
             </button>
-            <button  onClick={setSelectedSubject('Math')} class="selector_btn">
+            <button  onClick={()=>setSelectedSubject('Math')} className="selector_btn">
                 Math
             </button>
-            <button  onClick={setSelectedSubject('Sciences')} class="selector_btn">
+            <button  onClick={()=>setSelectedSubject('Sciences')} className="selector_btn">
                 Sciences
             </button>
         </div>
-        <h2 class="headings">Topic</h2>
-        <div class="heading_line"></div>
+        <h2 className="headings">Topic</h2>
+        <div className="heading_line"></div>
         {selectedSubject === 'Social Studies' && (
-            <div id="GEDSocialStudies" class="selector_btns_box">
-                <button onClick={selectTopics('All')} class="selected_selector_btn selector_btn">
+            <div id="GEDSocialStudies" className="selector_btns_box">
+                <button onClick={()=>selectTopics('All')} className="selected_selector_btn selector_btn">
                     All
                 </button>
-                <button onClick={selectTopics('Types of Government')} class="selector_btn">
+                <button onClick={()=>selectTopics('Types of Government')} className="selector_btn">
                     Types of Government
                 </button>
-                <button onClick={selectTopics('American Principal and Consitution')} class="selector_btn">
+                <button onClick={()=>selectTopics('American Principal and Consitution')} className="selector_btn">
                     American Principal and Consitution
                 </button>
-                <button onClick={selectTopics('American Goverment Strucutre')} class="selector_btn">
+                <button onClick={()=>selectTopics('American Goverment Strucutre')} className="selector_btn">
                     American Goverment Strucutre
                 </button>
-                <button onClick={selectTopics('American History')} class="selector_btn">
+                <button onClick={()=>selectTopics('American History')} className="selector_btn">
                     American History
                 </button>
             </div>
         )}
         {selectedSubject === 'Sciences' && (
-            <div id="GEDSciences" class="selector_btns_box">
-            <button onClick={selectTopics('All')} class="selected_selector_btn selector_btn">
+            <div id="GEDSciences" className="selector_btns_box">
+            <button onClick={()=>selectTopics('All')} className="selected_selector_btn selector_btn">
                 All
             </button>
-            <button onClick={selectTopics('Chemistry')} class="selector_btn">
+            <button onClick={()=>selectTopics('Chemistry')} className="selector_btn">
                 Chemistry
             </button>
-            <button onClick={selectTopics('Physics')} class="selector_btn">
+            <button onClick={()=>selectTopics('Physics')} className="selector_btn">
                 Physics
             </button>
-            <button onClick={selectTopics('Biology')} class="selector_btn">
+            <button onClick={()=>selectTopics('Biology')} className="selector_btn">
                 Biology
             </button>
-            <button onClick={selectTopics('Earth and Space')} class="selector_btn">
+            <button onClick={()=>selectTopics('Earth and Space')} className="selector_btn">
                 Earth and Space
             </button>
         </div>
         )}
         {selectedSubject === 'RLA' && (
-             <div id="GEDRLA" class="selector_btns_box">
-                <button onClick={selectTopics('All')} class="selected_selector_btn selector_btn">
+             <div id="GEDRLA" className="selector_btns_box">
+                <button onClick={()=>selectTopics('All')} className="selected_selector_btn selector_btn">
                     All
                 </button>
-                <button onClick={selectTopics('Fictional Reading Comprehension')} class="selector_btn">
+                <button onClick={()=>selectTopics('Fictional Reading Comprehension')} className="selector_btn">
                     Fictional Reading Comprehension
                 </button>
-                <button onClick={selectTopics('Non-Fictional Reading Comprehension')} class="selector_btn">
+                <button onClick={()=>selectTopics('Non-Fictional Reading Comprehension')} className="selector_btn">
                     Non-Fictional Reading Comprehension
                 </button>
-                <button onClick={selectTopics('Grammar')} class="selector_btn">
+                <button onClick={()=>selectTopics('Grammar')} className="selector_btn">
                     Grammar
                 </button>
             </div>
         )}
         {selectedSubject === 'Math' && (
-            <div id="GEDMath" class="selector_btns_box">
-                <button onClick={selectTopics('All')} class="selected_selector_btn selector_btn">
+            <div id="GEDMath" className="selector_btns_box">
+                <button onClick={()=>selectTopics('All')} className="selected_selector_btn selector_btn">
                     All
                 </button>
-                <button onClick={selectTopics('Percent & Rate')} class="selector_btn">
+                <button onClick={()=>selectTopics('Percent & Rate')} className="selector_btn">
                     Percent & Rate
                 </button>
-                <button onClick={selectTopics('Polynomial Expressions')} class="selector_btn">
+                <button onClick={()=>selectTopics('Polynomial Expressions')} className="selector_btn">
                     Polynomial Expressions
                 </button>
-                <button  onClick={selectTopics('Quadratic Equations')} class="selector_btn">
+                <button  onClick={()=>selectTopics('Quadratic Equations')} className="selector_btn">
                     Quadratic Equations
                 </button>
-                <button onClick={selectTopics('Graphs and Functions')} class="selector_btn">
+                <button onClick={()=>selectTopics('Graphs and Functions')} className="selector_btn">
                     Graphs and Functions
                 </button>
             </div>
@@ -179,35 +178,35 @@ const PracticeListPage = () => {
         
         {selectedSubject === 'Sciences' && (
             <>
-                <h2 class="headings">Sub-Topic</h2>
-                <div class="heading_line"></div>
+                <h2 className="headings">Sub-Topic</h2>
+                <div className="heading_line"></div>
                 {selectedTopics.includes('Chemistry') && (
-                    <div id="GEDScience_chemistry" class="selector_btns_box">
-                        <button onClick={selectSubTopics('All')} class="selected_selector_btn selector_btn">
+                    <div id="GEDScience_chemistry" className="selector_btns_box">
+                        <button onClick={selectSubTopics('All')} className="selected_selector_btn selector_btn">
                             All
                         </button>
                     </div>
                     )
                 }
                 {selectedTopics.includes('Biology') && (
-                    <div id="GEDScience_biology" class="selector_btns_box">
-                        <button onClick={selectSubTopics('All')} class="selected_selector_btn selector_btn">
+                    <div id="GEDScience_biology" className="selector_btns_box">
+                        <button onClick={selectSubTopics('All')} className="selected_selector_btn selector_btn">
                             All
                         </button>
                     </div>
                     )
                 }
                 {selectedTopics.includes('Physics') && (
-                    <div id="GEDScience_physics" class="selector_btns_box">
-                        <button onClick={selectSubTopics('All')} class="selected_selector_btn selector_btn">
+                    <div id="GEDScience_physics" className="selector_btns_box">
+                        <button onClick={selectSubTopics('All')} className="selected_selector_btn selector_btn">
                             All
                         </button>
                     </div>
                     )
                 }
                 {selectedTopics.includes('EarthAndScience') && (
-                    <div id="GEDScience_EarthAndSpace" class="selector_btns_box">
-                        <button onClick={selectSubTopics('All')} class="selected_selector_btn selector_btn">
+                    <div id="GEDScience_EarthAndSpace" className="selector_btns_box">
+                        <button onClick={selectSubTopics('All')} className="selected_selector_btn selector_btn">
                             All
                         </button>
                     </div>
@@ -218,7 +217,7 @@ const PracticeListPage = () => {
         
         
        
-        <div class="quiz_select_box">
+        <div className="quiz_select_box">
             {quizzes.map((quiz, index)=>(
                 <InfoCard key={index} quiz={quiz}/>
             ))}
