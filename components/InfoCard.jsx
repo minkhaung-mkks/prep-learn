f6d4e7051a2a16cf826210d73dcab240cbb0022f
Min Khaung <minkhaung.mkks@gmail.com>
2023-06-14 22:09:03 +0630

    Changed Class to className

--- components/InfoCard.jsx
+++ components/InfoCard.jsx
@@ -8,45 +8,45 @@ const InfoCard = ({index,quiz}) => {
         router.push(`/practices/${id}`)
     }
     return (
-        <div class="quiz_card">
+        <div className="quiz_card">
                 
-                <div class="creator_info_box">
-                    <Image width={50} height={50} src="/assets/preplearn-website-favicon-color.png" alt="" class="creator_img"/>
+                <div className="creator_info_box">
+                    <Image width={50} height={50} src="/assets/preplearn-website-favicon-color.png" alt="" className="creator_img"/>
                     
-                    <a href={`../users/{quiz.creatorID}`} class="creator_name">{quiz.creator}</a>
-                    <h5 class="creator_type"></h5>
-                    <h4 class="quiz_type"> {quiz.source}</h4>
+                    <a href={`../users/{quiz.creatorID}`} className="creator_name">{quiz.creator}</a>
+                    <h5 className="creator_type"></h5>
+                    <h4 className="quiz_type"> {quiz.source}</h4>
                 </div>
                 
-                <div class="quiz_info_box">
+                <div className="quiz_info_box">
                     
-                    <div class="quiz_title_box">
+                    <div className="quiz_title_box">
                        
-                        <h2 class="quiz_title">{quiz.name}</h2>
+                        <h2 className="quiz_title">{quiz.name}</h2>
                        
-                        <h3 class="quiz_subject">Subject : {quiz.subject}</h3>
+                        <h3 className="quiz_subject">Subject : {quiz.subject}</h3>
                         
-                        <h3 class="quiz_topic">Topic : {quiz.topic}</h3>
+                        <h3 className="quiz_topic">Topic : {quiz.topic}</h3>
                         {quiz.sub_topic !== "" && 
-                            <h3 class="quiz_topic">Sub Topic {quiz.sub_topic}</h3>
+                            <h3 className="quiz_topic">Sub Topic {quiz.sub_topic}</h3>
                         }
                     </div>
                     
                 </div>
-                <div class="quiz_info_box">
+                <div className="quiz_info_box">
                     
-                    <div class="quiz_title_box">
+                    <div className="quiz_title_box">
                         
-                        <h3 class="quiz_title">Difficulty : {quiz.difficulty}</h3>
+                        <h3 className="quiz_title">Difficulty : {quiz.difficulty}</h3>
                        
-                        <h3 class="quiz_subject">Questions : {quiz.total_questions} questions</h3>
+                        <h3 className="quiz_subject">Questions : {quiz.total_questions} questions</h3>
                         
-                        <h3 class="quiz_topic"> Ideal Time : {quiz.ideal_time} Mins</h3>
+                        <h3 className="quiz_topic"> Ideal Time : {quiz.ideal_time} Mins</h3>
                     </div>
                     
                 </div>
-                <div class="take_quiz_btn_box">
-                    <button onClick={()=>takeQuiz(quiz.id)} class="take_quiz_btn">
+                <div className="take_quiz_btn_box">
+                    <button onClick={()=>takeQuiz(quiz.id)} className="take_quiz_btn">
                         Start
                     </button>
                 </div>
