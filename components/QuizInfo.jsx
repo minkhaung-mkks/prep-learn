649f4f3c91dff600372a70679540924f5af0426e
Min Khaung <minkhaung.mkks@gmail.com>
2023-06-19 21:36:50 +0630

    All the necessary Topics,Subtopics and Subjects

--- components/QuizInfo.jsx
+++ components/QuizInfo.jsx
@@ -25,7 +25,9 @@ const QuizInfo = ({ quizInfoState, updateQuizInfoState, nextPage }) => {
     setQuizSubtopic(updatedQuizSubtopic);
     updateQuizInfoState({ ...quizInfoState, sub_topic: updatedQuizSubtopic });
   };
-
+  const handleSubjectClick = (subject) => {
+    updateQuizInfoState({ ...quizInfoState, subject: subject })
+  }
   const handleNext = () => {
     // Validate the quiz information fields
     // ...
@@ -43,24 +45,39 @@ const QuizInfo = ({ quizInfoState, updateQuizInfoState, nextPage }) => {
         onChange={(e) => updateQuizInfoState({ ...quizInfoState, name: e.target.value })}
         placeholder="Quiz Name"
       />
-      <input
-        type="text"
-        value={quizInfoState.quizSubject}
-        onChange={(e) => updateQuizInfoState({ ...quizInfoState, subject: e.target.value })}
-        placeholder="Quiz Subject"
-      />
+      <h2>Subjects</h2>
+      <button onClick={() => handleSubjectClick('RLA')}>
+        RLA
+      </button>
+      <button onClick={() => handleSubjectClick('Social Studies')}>
+        Social Studies
+      </button>
+      <button onClick={() => handleSubjectClick('Sciences')}>
+        Sciences
+      </button>
       <div>
         <h4>Quiz Topics:</h4>
-        <button onClick={() => handleTopicClick('government')}>
-          Government
-        </button>
-        <button onClick={() => handleTopicClick('history')}>History</button>
-        <button onClick={() => handleTopicClick('economy')}>Economy</button>
+        <h5>Social Studies</h5>
+        <button onClick={() => handleTopicClick('Civics & Government')}>Civics & Government</button>
+        <button onClick={() => handleTopicClick('American Principals')}>American Principals</button>
+        <button onClick={() => handleTopicClick('American History')}>American History</button>
+        <button onClick={() => handleTopicClick('Famouse Speeches')}>Famouse Speeches</button>
+        <button onClick={() => handleTopicClick('World History')}>World History</button>
+        <h5>Sciences</h5>
+        <button onClick={() => handleTopicClick('Chemistry')}>Chemistry</button>
+        <button onClick={() => handleTopicClick('Biology')}>Biology</button>
+        <button onClick={() => handleTopicClick('Physics')}>Physics</button>
+        <button onClick={() => handleTopicClick('Earth & Space')}>Earth & Space</button>
+        <h5>RLA</h5>
+        <button onClick={() => handleTopicClick('Fictional Reading Comprehension')}>Fictional Reading Comprehension</button>
+        <button onClick={() => handleTopicClick('Non-Fictional Reading Comprehension')}>Non-Fictional Reading Comprehension</button>
+        <button onClick={() => handleTopicClick('Grammar')}>Grammar</button>
+        <button onClick={() => handleTopicClick('Vocabulary')}>Vocabulary</button>
       </div>
       <div>
         <h4>Quiz Subtopics:</h4>
-        <button onClick={() => handleSubtopicClick('greek')}>Greek</button>
-        <button onClick={() => handleSubtopicClick('US')}>US</button>
+        <button onClick={() => handleSubtopicClick('chemistry')}>chemistry</button>
+        <button onClick={() => handleSubtopicClick('biology')}>biology</button>
       </div>
       <input
         type="text"
