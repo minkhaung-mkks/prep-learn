2633c9b38dcdc802cf220099ca53e3a75c95a26f
Min Khaung <minkhaung.mkks@gmail.com>
2023-06-19 21:47:07 +0630

    Preped for CSS

--- components/QuizInfo.jsx
+++ components/QuizInfo.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 const QuizInfo = ({ quizInfoState, updateQuizInfoState, nextPage }) => {
   const [quizTopic, setQuizTopic] = useState([]);
+  const [quizSubject, setQuizSubject] = useState('')
   const [quizSubtopic, setQuizSubtopic] = useState([]);
 
   const handleTopicClick = (topic) => {
@@ -27,6 +28,7 @@ const QuizInfo = ({ quizInfoState, updateQuizInfoState, nextPage }) => {
   };
   const handleSubjectClick = (subject) => {
     updateQuizInfoState({ ...quizInfoState, subject: subject })
+    setQuizSubject(subject)
   }
   const handleNext = () => {
     // Validate the quiz information fields
@@ -37,74 +39,79 @@ const QuizInfo = ({ quizInfoState, updateQuizInfoState, nextPage }) => {
   };
 
   return (
-    <div>
+    <div className='quiz_form'>
       <h2>Quiz Information</h2>
       <input
         type="text"
         value={quizInfoState.quizName}
         onChange={(e) => updateQuizInfoState({ ...quizInfoState, name: e.target.value })}
+        className='input_fields'
         placeholder="Quiz Name"
       />
       <h2>Subjects</h2>
-      <button onClick={() => handleSubjectClick('RLA')}>
+      <button className={`selectingBtn ${quizSubject === 'RLA' ? 'selectedBtn' : ''} `} onClick={() => handleSubjectClick('RLA')}>
         RLA
       </button>
-      <button onClick={() => handleSubjectClick('Social Studies')}>
+      <button className={`selectingBtn ${quizSubject === 'Social Studies' ? 'selectedBtn' : ''} `} onClick={() => handleSubjectClick('Social Studies')}>
         Social Studies
       </button>
-      <button onClick={() => handleSubjectClick('Sciences')}>
+      <button className={`selectingBtn ${quizSubject === 'Sciences' ? 'selectedBtn' : ''} `} onClick={() => handleSubjectClick('Sciences')}>
         Sciences
       </button>
       <div>
         <h4>Quiz Topics:</h4>
         <h5>Social Studies</h5>
-        <button onClick={() => handleTopicClick('Civics & Government')}>Civics & Government</button>
-        <button onClick={() => handleTopicClick('American Principals')}>American Principals</button>
-        <button onClick={() => handleTopicClick('American History')}>American History</button>
-        <button onClick={() => handleTopicClick('Famouse Speeches')}>Famouse Speeches</button>
-        <button onClick={() => handleTopicClick('World History')}>World History</button>
+        <button className={`selectingBtn ${quizTopic.includes(`Civics & Government`) ? 'selectedBtn' : ''} `} onClick={() => handleTopicClick('Civics & Government')}>Civics & Government</button>
+        <button className={`selectingBtn ${quizTopic.includes(`American Principals`) ? 'selectedBtn' : ''} `} onClick={() => handleTopicClick('American Principals')}>American Principals</button>
+        <button className={`selectingBtn ${quizTopic.includes(`American History`) ? 'selectedBtn' : ''} `} onClick={() => handleTopicClick('American History')}>American History</button>
+        <button className={`selectingBtn ${quizTopic.includes(`Famouse Speeches`) ? 'selectedBtn' : ''} `} onClick={() => handleTopicClick('Famouse Speeches')}>Famouse Speeches</button>
+        <button className={`selectingBtn ${quizTopic.includes(`World History`) ? 'selectedBtn' : ''} `} onClick={() => handleTopicClick('World History')}>World History</button>
         <h5>Sciences</h5>
-        <button onClick={() => handleTopicClick('Chemistry')}>Chemistry</button>
-        <button onClick={() => handleTopicClick('Biology')}>Biology</button>
-        <button onClick={() => handleTopicClick('Physics')}>Physics</button>
-        <button onClick={() => handleTopicClick('Earth & Space')}>Earth & Space</button>
+        <button className={`selectingBtn ${quizTopic.includes(`Chemistry`) ? 'selectedBtn' : ''} `} onClick={() => handleTopicClick('Chemistry')}>Chemistry</button>
+        <button className={`selectingBtn ${quizTopic.includes(`Biology`) ? 'selectedBtn' : ''} `} onClick={() => handleTopicClick('Biology')}>Biology</button>
+        <button className={`selectingBtn ${quizTopic.includes(`Physics`) ? 'selectedBtn' : ''} `} onClick={() => handleTopicClick('Physics')}>Physics</button>
+        <button className={`selectingBtn ${quizTopic.includes(`Earth & Space`) ? 'selectedBtn' : ''} `} onClick={() => handleTopicClick('Earth & Space')}>Earth & Space</button>
         <h5>RLA</h5>
-        <button onClick={() => handleTopicClick('Fictional Reading Comprehension')}>Fictional Reading Comprehension</button>
-        <button onClick={() => handleTopicClick('Non-Fictional Reading Comprehension')}>Non-Fictional Reading Comprehension</button>
-        <button onClick={() => handleTopicClick('Grammar')}>Grammar</button>
-        <button onClick={() => handleTopicClick('Vocabulary')}>Vocabulary</button>
+        <button className={`selectingBtn ${quizTopic.includes(`Fictional Reading Comprehension`) ? 'selectedBtn' : ''} `} onClick={() => handleTopicClick('Fictional Reading Comprehension')}>Fictional Reading Comprehension</button>
+        <button className={`selectingBtn ${quizTopic.includes(`Non-Fictional Reading Comprehension`) ? 'selectedBtn' : ''} `} onClick={() => handleTopicClick('Non-Fictional Reading Comprehension')}>Non-Fictional Reading Comprehension</button>
+        <button className={`selectingBtn ${quizTopic.includes(`Grammar`) ? 'selectedBtn' : ''} `} onClick={() => handleTopicClick('Grammar')}>Grammar</button>
+        <button className={`selectingBtn ${quizTopic.includes(`Vocabulary`) ? 'selectedBtn' : ''} `} onClick={() => handleTopicClick('Vocabulary')}>Vocabulary</button>
       </div>
       <div>
         <h4>Quiz Subtopics:</h4>
-        <button onClick={() => handleSubtopicClick('chemistry')}>chemistry</button>
-        <button onClick={() => handleSubtopicClick('biology')}>biology</button>
+        <button className={`selectingBtn ${quizSubtopic.includes(`chemistry`) ? 'selectedBtn' : ''} `} onClick={() => handleSubtopicClick('chemistry')}>chemistry</button>
+        <button className={`selectingBtn ${quizSubtopic.includes(`biology`) ? 'selectedBtn' : ''} `} onClick={() => handleSubtopicClick('biology')}>biology</button>
       </div>
       <input
         type="text"
         value={quizInfoState.difficulty}
         onChange={(e) => updateQuizInfoState({ ...quizInfoState, difficulty: e.target.value })}
         placeholder="Difficulty"
+        className='input_fields'
       />
       <input
         type="text"
         value={quizInfoState.idealTime}
         onChange={(e) => updateQuizInfoState({ ...quizInfoState, idealTime: e.target.value })}
         placeholder="Ideal Time"
+        className='input_fields'
       />
       <input
         type="text"
         value={quizInfoState.creator}
         onChange={(e) => updateQuizInfoState({ ...quizInfoState, creator: e.target.value })}
         placeholder="Creator"
+        className='input_fields'
       />
       <input
         type="text"
         value={quizInfoState.creatorID}
         onChange={(e) => updateQuizInfoState({ ...quizInfoState, creatorID: e.target.value })}
         placeholder="Creator ID"
+        className='input_fields'
       />
 
-      <button onClick={handleNext}>Next</button>
+      <button className='next_btn' onClick={handleNext}>Next</button>
       <button onClick={() => console.log(quizInfoState)}>Tests</button>
     </div>
   );
