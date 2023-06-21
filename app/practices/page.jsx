987df40aae8bfe003b8144982b2551fa468be015
Min Khaung <minkhaung.mkks@gmail.com>
2023-06-21 15:54:17 +0630

    Made it so that CSS updates when the button is clicked to search

--- app/practices/page.jsx
+++ app/practices/page.jsx
@@ -81,19 +81,19 @@ const PracticeListPage = () => {
             <h2 className="headings">Subject</h2>
             <div className="heading_line"></div>
             <div id="GED" className="selector_btns_box">
-                <button onClick={() => setSelectedSubject('All')} className="selected_selector_btn selector_btn">
+                <button onClick={() => setSelectedSubject('All')} className={`selector_btn ${selectedSubject === 'All' ? "selected_selector_btn" : ""}`}>
                     All
                 </button>
-                <button onClick={() => setSelectedSubject('Social Studies')} className="selector_btn">
+                <button onClick={() => setSelectedSubject('Social Studies')} className={`selector_btn ${selectedSubject === 'Social Studies' ? "selected_selector_btn" : ""}`}>
                     Social Studies
                 </button>
-                <button onClick={() => setSelectedSubject('RLA')} className="selector_btn">
+                <button onClick={() => setSelectedSubject('RLA')} className={`selector_btn ${selectedSubject === 'RLA' ? "selected_selector_btn" : ""}`}>
                     Reasoning through Art
                 </button>
-                <button onClick={() => setSelectedSubject('Math')} className="selector_btn">
+                <button onClick={() => setSelectedSubject('Math')} className={`selector_btn ${selectedSubject === 'Math' ? "selected_selector_btn" : ""}`}>
                     Math
                 </button>
-                <button onClick={() => setSelectedSubject('Sciences')} className="selector_btn">
+                <button onClick={() => setSelectedSubject('Sciences')} className={`selector_btn ${selectedSubject === 'Sciences' ? "selected_selector_btn" : ""}`}>
                     Sciences
                 </button>
             </div>
@@ -101,73 +101,76 @@ const PracticeListPage = () => {
             <div className="heading_line"></div>
             {selectedSubject === 'Social Studies' && (
                 <div id="GEDSocialStudies" className="selector_btns_box">
-                    <button onClick={() => selectTopics('All')} className="selected_selector_btn selector_btn">
+                    <button onClick={() => selectTopics('All')} className={`selector_btn ${selectedTopics === 'All' ? "selected_selector_btn" : ""}`}>
                         All
                     </button>
-                    <button onClick={() => selectTopics('Types of Government')} className="selector_btn">
-                        Types of Government
+                    <button onClick={() => selectTopics('Civics & Government')} className={`selector_btn ${selectedTopics === 'Civics & Government' ? "selected_selector_btn" : ""}`}>
+                        Civics & Government
                     </button>
-                    <button onClick={() => selectTopics('American Principal and Consitution')} className="selector_btn">
-                        American Principal and Consitution
+                    <button onClick={() => selectTopics('American Principals')} className={`selector_btn ${selectedTopics === 'American Principals' ? "selected_selector_btn" : ""}`}>
+                        American Principals
                     </button>
-                    <button onClick={() => selectTopics('American Goverment Strucutre')} className="selector_btn">
+                    <button onClick={() => selectTopics('American Goverment Strucutre')} className={`selector_btn ${selectedTopics === 'American Goverment Strucutre' ? "selected_selector_btn" : ""}`}>
                         American Goverment Strucutre
                     </button>
-                    <button onClick={() => selectTopics('American History')} className="selector_btn">
+                    <button onClick={() => selectTopics('American History')} className={`selector_btn ${selectedTopics === 'American History' ? "selected_selector_btn" : ""}`}>
                         American History
+                    </button>
+                    <button onClick={() => selectTopics('World History')} className={`selector_btn ${selectedTopics === 'World History' ? "selected_selector_btn" : ""}`}>
+                        World History
                     </button>
                 </div>
             )}
             {selectedSubject === 'Sciences' && (
                 <div id="GEDSciences" className="selector_btns_box">
-                    <button onClick={() => selectTopics('All')} className="selected_selector_btn selector_btn">
+                    <button onClick={() => selectTopics('All')} className={`selector_btn ${selectedTopics === 'All' ? "selected_selector_btn" : ""}`}>
                         All
                     </button>
-                    <button onClick={() => selectTopics('Chemistry')} className="selector_btn">
+                    <button onClick={() => selectTopics('Chemistry')} className={`selector_btn ${selectedTopics === 'Chemistry' ? "selected_selector_btn" : ""}`}>
                         Chemistry
                     </button>
-                    <button onClick={() => selectTopics('Physics')} className="selector_btn">
+                    <button onClick={() => selectTopics('Physics')} className={`selector_btn ${selectedTopics === 'Physics' ? "selected_selector_btn" : ""}`}>
                         Physics
                     </button>
-                    <button onClick={() => selectTopics('Biology')} className="selector_btn">
+                    <button onClick={() => selectTopics('Biology')} className={`selector_btn ${selectedTopics === 'Biology' ? "selected_selector_btn" : ""}`}>
                         Biology
                     </button>
-                    <button onClick={() => selectTopics('Earth and Space')} className="selector_btn">
+                    <button onClick={() => selectTopics('Earth and Space')} className={`selector_btn ${selectedTopics === 'Earth and Space' ? "selected_selector_btn" : ""}`}>
                         Earth and Space
                     </button>
                 </div>
             )}
             {selectedSubject === 'RLA' && (
                 <div id="GEDRLA" className="selector_btns_box">
-                    <button onClick={() => selectTopics('All')} className="selected_selector_btn selector_btn">
+                    <button onClick={() => selectTopics('All')} className={`selector_btn ${selectedTopics === 'All' ? "selected_selector_btn" : ""}`}>
                         All
                     </button>
-                    <button onClick={() => selectTopics('Fictional Reading Comprehension')} className="selector_btn">
+                    <button onClick={() => selectTopics('Fictional Reading Comprehension')} className={`selector_btn ${selectedTopics === 'Fictional Reading Comprehension' ? "selected_selector_btn" : ""}`}>
                         Fictional Reading Comprehension
                     </button>
-                    <button onClick={() => selectTopics('Non-Fictional Reading Comprehension')} className="selector_btn">
+                    <button onClick={() => selectTopics('Non-Fictional Reading Comprehension')} className={`selector_btn ${selectedTopics === 'Non-Fictional Reading Comprehension' ? "selected_selector_btn" : ""}`}>
                         Non-Fictional Reading Comprehension
                     </button>
-                    <button onClick={() => selectTopics('Grammar')} className="selector_btn">
+                    <button onClick={() => selectTopics('Grammar')} className={`selector_btn ${selectedTopics === 'Grammar' ? "selected_selector_btn" : ""}`}>
                         Grammar
                     </button>
                 </div>
             )}
             {selectedSubject === 'Math' && (
                 <div id="GEDMath" className="selector_btns_box">
-                    <button onClick={() => selectTopics('All')} className="selected_selector_btn selector_btn">
+                    <button onClick={() => selectTopics('All')} className={`selector_btn ${selectedTopics === 'All' ? "selected_selector_btn" : ""}`}>
                         All
                     </button>
-                    <button onClick={() => selectTopics('Percent & Rate')} className="selector_btn">
+                    <button onClick={() => selectTopics('Percent & Rate')} className={`selector_btn ${selectedTopics === 'Percent & Rate' ? "selected_selector_btn" : ""}`}>
                         Percent & Rate
                     </button>
-                    <button onClick={() => selectTopics('Polynomial Expressions')} className="selector_btn">
+                    <button onClick={() => selectTopics('Polynomial Expressions')} className={`selector_btn ${selectedTopics === 'Polynomial Expressions' ? "selected_selector_btn" : ""}`}>
                         Polynomial Expressions
                     </button>
-                    <button onClick={() => selectTopics('Quadratic Equations')} className="selector_btn">
+                    <button onClick={() => selectTopics('Quadratic Equations')} className={`selector_btn ${selectedTopics === 'Quadratic Equations' ? "selected_selector_btn" : ""}`}>
                         Quadratic Equations
                     </button>
-                    <button onClick={() => selectTopics('Graphs and Functions')} className="selector_btn">
+                    <button onClick={() => selectTopics('Graphs and Functions')} className={`selector_btn ${selectedTopics === 'Graphs and Functions' ? "selected_selector_btn" : ""}`}>
                         Graphs and Functions
                     </button>
                 </div>
@@ -179,7 +182,7 @@ const PracticeListPage = () => {
                     <div className="heading_line"></div>
                     {selectedTopics.includes('Chemistry') && (
                         <div id="GEDScience_chemistry" className="selector_btns_box">
-                            <button onClick={selectSubTopics('All')} className="selected_selector_btn selector_btn">
+                            <button onClick={selectSubTopics('All')} className={`selector_btn ${selectedSubTopics === 'All' ? "selected_selector_btn" : ""}`}>
                                 All
                             </button>
                         </div>
@@ -187,7 +190,7 @@ const PracticeListPage = () => {
                     }
                     {selectedTopics.includes('Biology') && (
                         <div id="GEDScience_biology" className="selector_btns_box">
-                            <button onClick={selectSubTopics('All')} className="selected_selector_btn selector_btn">
+                            <button onClick={selectSubTopics('All')} className={`selector_btn ${selectedSubTopics === 'All' ? "selected_selector_btn" : ""}`}>
                                 All
                             </button>
                         </div>
@@ -195,7 +198,7 @@ const PracticeListPage = () => {
                     }
                     {selectedTopics.includes('Physics') && (
                         <div id="GEDScience_physics" className="selector_btns_box">
-                            <button onClick={selectSubTopics('All')} className="selected_selector_btn selector_btn">
+                            <button onClick={selectSubTopics('All')} className={`selector_btn ${selectedSubTopics === 'All' ? "selected_selector_btn" : ""}`}>
                                 All
                             </button>
                         </div>
@@ -203,7 +206,7 @@ const PracticeListPage = () => {
                     }
                     {selectedTopics.includes('EarthAndScience') && (
                         <div id="GEDScience_EarthAndSpace" className="selector_btns_box">
-                            <button onClick={selectSubTopics('All')} className="selected_selector_btn selector_btn">
+                            <button onClick={selectSubTopics('All')} className={`selector_btn ${selectedSubTopics === 'All' ? "selected_selector_btn" : ""}`}>
                                 All
                             </button>
                         </div>
