116fec7c621f7216424f56b9be50b55f016ac525
Min Khaung <minkhaung.mkks@gmail.com>
2023-06-21 22:12:05 +0630

    Bug fix and Improvements

--- components/QuestionCard.jsx
+++ components/QuestionCard.jsx
@@ -3,7 +3,7 @@ const QuestionCard = ({ question, qIndex, index, toggleRadio, answer, hasSubmitt
     <div className="question_card">
       <h3 className="question">{`${qIndex + 1} ${question.text}`}</h3>
       <div className="answers_box">
-        <div onClick={(e) => toggleRadio(e, index, qIndex)} className={`radio_container ${hasSubmitted ? "noClick" : ""} ${answer === "a" ? `checked_container ${hasSubmitted && question.correct !== 'a' ? "wrongBox" : ""}` : ""} ${hasSubmitted && question.correct === 'a' ? "correctBox checked_container" : ""}`}>
+        <div onClick={(e) => toggleRadio(e, index, qIndex)} className={`radio_container ${hasSubmitted ? "noClick" : ""} ${answer === "a" ? `checked_container ${hasSubmitted && question.correct.toLowerCase() !== 'a' ? "wrongBox" : ""}` : ""} ${hasSubmitted && question.correct.toLowerCase() === 'a' ? "correctBox checked_container" : ""}`}>
           <input
             className="answers"
             type="radio"
@@ -14,7 +14,7 @@ const QuestionCard = ({ question, qIndex, index, toggleRadio, answer, hasSubmitt
           />
           <label htmlFor="A">{question.answers.a}</label>
         </div>
-        <div onClick={(e) => toggleRadio(e, index, qIndex)} className={`radio_container ${hasSubmitted ? "noClick" : ""} ${answer === "b" ? `checked_container ${hasSubmitted && question.correct !== 'b' ? "wrongBox" : ""}` : ""} ${hasSubmitted && question.correct === 'b' ? "correctBox checked_container" : ""}`}>
+        <div onClick={(e) => toggleRadio(e, index, qIndex)} className={`radio_container ${hasSubmitted ? "noClick" : ""} ${answer === "b" ? `checked_container ${hasSubmitted && question.correct.toLowerCase() !== 'b' ? "wrongBox" : ""}` : ""} ${hasSubmitted && question.correct.toLowerCase() === 'b' ? "correctBox checked_container" : ""}`}>
           <input
             className="answers"
             type="radio"
@@ -25,7 +25,7 @@ const QuestionCard = ({ question, qIndex, index, toggleRadio, answer, hasSubmitt
           />
           <label htmlFor="B">{question.answers.b}</label>
         </div>
-        <div onClick={(e) => toggleRadio(e, index, qIndex)} className={`radio_container ${hasSubmitted ? "noClick" : ""} ${answer === "c" ? `checked_container ${hasSubmitted && question.correct !== 'c' ? "wrongBox" : ""}` : ""} ${hasSubmitted && question.correct === 'c' ? "correctBox checked_container" : ""}`}>
+        <div onClick={(e) => toggleRadio(e, index, qIndex)} className={`radio_container ${hasSubmitted ? "noClick" : ""} ${answer === "c" ? `checked_container ${hasSubmitted && question.correct.toLowerCase() !== 'c' ? "wrongBox" : ""}` : ""} ${hasSubmitted && question.correct.toLowerCase() === 'c' ? "correctBox checked_container" : ""}`}>
           <input
             className="answers"
             type="radio"
@@ -36,7 +36,7 @@ const QuestionCard = ({ question, qIndex, index, toggleRadio, answer, hasSubmitt
           />
           <label htmlFor="C">{question.answers.c}</label>
         </div>
-        <div onClick={(e) => toggleRadio(e, index, qIndex)} className={`radio_container ${hasSubmitted ? "noClick" : ""} ${answer === "d" ? `checked_container ${hasSubmitted && question.correct !== 'd' ? "wrongBox" : ""}` : ""} ${hasSubmitted && question.correct === 'd' ? "correctBox checked_container" : ""}`}>
+        <div onClick={(e) => toggleRadio(e, index, qIndex)} className={`radio_container ${hasSubmitted ? "noClick" : ""} ${answer === "d" ? `checked_container ${hasSubmitted && question.correct.toLowerCase() !== 'd' ? "wrongBox" : ""}` : ""} ${hasSubmitted && question.correct.toLowerCase() === 'd' ? "correctBox checked_container" : ""}`}>
           <input
             className="answers"
             type="radio"
@@ -49,7 +49,7 @@ const QuestionCard = ({ question, qIndex, index, toggleRadio, answer, hasSubmitt
         </div>
       </div>
       {hasSubmitted &&
-        <div className={`correct_box ${answer === question.correct ? 'correct_answer' : 'wrong_answer'}`}>
+        <div className={`correct_box ${answer === question.correct.toLowerCase() ? 'correct_answer' : 'wrong_answer'}`}>
           <h3 className="provided_answer">
             {question.correct.toUpperCase() + '. ' + question.answer_text}
           </h3>
