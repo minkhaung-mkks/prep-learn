10af48aa9bb5a0a8bf4c34e1494c0b08bb338251
Min Khaung <minkhaung.mkks@gmail.com>
2023-08-13 18:31:44 +0630

    Logic and Visual Planning of the new question maker

--- components/NewQuestionCard.jsx
+++ components/NewQuestionCard.jsx
@@ -6,4 +6,25 @@ const NewQuestionCard = () => {
     )
 }
 
+// Visual
+// Context Field
+// Questions that will be mapped over
+// Q1-....>
+// New Question Card
+// Question text
+// Answer Options
+//  A1
+//  A2....
+// Correct Option Selector
+// Reason
+
+// How the question logic should work
+// Q TEXT /n
+// AO1 /n
+// AO2 /n
+// AO3 /n
+// AO4 (Correct) * /n
+// - Reason.
+// 
+
 export default NewQuestionCard
