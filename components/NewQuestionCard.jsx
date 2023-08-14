37d51351f7434fe170a47d242e88f59410105424
Min Khaung <minkhaung.mkks@gmail.com>
2023-08-14 16:21:50 +0630

    Base Logic of Mapping over created Questions Done

--- components/NewQuestionCard.jsx
+++ components/NewQuestionCard.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
+import { parseQuestions } from '@utlis/Quiz_creation'
+import { useState, useEffect } from 'react'
 
 const NewQuestionCard = () => {
+
+    const [currentQuestion, setCurrentQuestion] = useState('')
+    const [questions, setQuestions] = useState([])
+    const [convertedQuestions, setConvertedQuestions] = useState([])
+    const newQuestion = () => {
+        setQuestions((prev) => [...prev, currentQuestion])
+    }
+    const convertQuestion = useCallback(async () => {
+        const newQuestions = await parseQuestions(JSON.stringify(questions));
+        return newQuestions;
+    }, [questions]);
+    useEffect(() => {
+        const convertData = async () => {
+            const convertedQuestions = await convertQuestion();
+            setConvertedQuestions(convertedQuestions);
+        };
+        convertData();
+    }, [convertQuestion])
     return (
-        <div>NewQuestionCard</div>
+        <>
+            {convertedQuestions.map((convertedQuestion, index) => (
+                <div key={index}>
+                    <h2 className="questionText">
+                        {convertedQuestion.text}
+                    </h2>
+                    {Object.keys(convertedQuestion.answers).map(answerKey => (
+                        <p key={answerKey} className={`answerText ${answerKey === convertedQuestion.correct ? 'correctOption' : ''}`}>
+                            {convertedQuestion.answers[answerKey]}
+                        </p>
+                    ))}
+                    <p className='reasonText'>
+                        {convertedQuestion.reason}
+                    </p>
+                </div>
+            ))}
+        </>
     )
 }
+
+
 
 // Visual
 // Context Field
