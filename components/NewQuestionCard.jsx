c1de6612d68e51e688a3bf8aa0921d81cb41c0e7
Min Khaung <minkhaung.mkks@gmail.com>
2023-08-14 16:44:38 +0630

    Base Logic of Adding Answer Options and Also Removing them

--- components/NewQuestionCard.jsx
+++ components/NewQuestionCard.jsx
@@ -4,11 +4,44 @@ import { useState, useEffect } from 'react'
 const NewQuestionCard = () => {
 
     const [currentQuestion, setCurrentQuestion] = useState('')
+    const [answerOptions, setAnswerOptions] = useState({});
+    const [correctOption, setCorrectOption] = useState('');
     const [questions, setQuestions] = useState([])
     const [convertedQuestions, setConvertedQuestions] = useState([])
     const newQuestion = () => {
         setQuestions((prev) => [...prev, currentQuestion])
     }
+    const handleAddOption = () => {
+        const nextOptionIndex = Object.keys(answerOptions).length;
+        const nextOptionLetter = alphabet[nextOptionIndex];
+        setAnswerOptions({
+            ...answerOptions,
+            [nextOptionLetter]: ''
+        });
+    };
+    const handleRemoveOption = (optionLetter) => {
+        const updatedOptions = { ...answerOptions };
+        delete updatedOptions[optionLetter];
+        setAnswerOptions(updatedOptions);
+
+        if (correctOption === optionLetter) {
+            setCorrectOption('');
+        }
+    };
+    const handleOptionChange = (optionLetter, newText) => {
+        setAnswerOptions({
+            ...answerOptions,
+            [optionLetter]: newText
+        });
+    };
+    const handleSelectCorrectOption = (optionLetter) => {
+        const optionText = answerOptions[optionLetter];
+        setCorrectOption(optionLetter);
+        setAnswerOptions({
+            ...answerOptions,
+            [optionLetter]: optionText + '*'
+        });
+    };
     const convertQuestion = useCallback(async () => {
         const newQuestions = await parseQuestions(JSON.stringify(questions));
         return newQuestions;
@@ -37,6 +70,24 @@ const NewQuestionCard = () => {
                     </p>
                 </div>
             ))}
+            <div>
+                <h2>Question: <span> <input type={'text'} className={'input_box'} value={currentQuestionText} onChange={(e) => setCurrentQuestion(e.target.value)}></input> </span></h2>
+                <h4>Answers</h4>
+                {Object.keys(answerOptions).map((optionLetter) => (
+                    <div key={optionLetter}>
+                        <input
+                            type="text"
+                            className='input_box'
+                            value={answerOptions[optionLetter]}
+                            onChange={(e) => handleOptionChange(optionLetter, e.target.value)}
+                        />
+                        {optionLetter === correctOption && <span> ✅ </span>}
+                        <button onClick={() => handleRemoveOption(optionLetter)}>Remove</button>
+                        <button onClick={() => handleSelectCorrectOption(optionLetter)}>Mark as Correct</button>
+                    </div>
+                ))}
+                <button onClick={handleAddOption}>Add Option</button>
+            </div>
         </>
     )
 }
